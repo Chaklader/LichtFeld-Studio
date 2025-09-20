b9fa9a81bb6c025a8ca50dbce0cde5698bb21a6b
Chaklader Asfak Arefe <omi.chaklader@gmail.com>
2025-09-20 12:25:46 +0600

    config: add 15k checkpoint to eval/save steps and update training script params

--- tools/train.sh
+++ tools/train.sh
@@ -19,19 +19,20 @@ MIN_OPACITY=0.005
 
 # Run the optimized training command
 ./build/LichtFeld-Studio \
-    -d ${DATASET_PATH} \
-    -o ${OUTPUT_PATH} \
-    --eval \
-    --save-eval-images \
-    --render-mode RGB \
-    -i ${ITERATIONS} \
-    --gut \
-    --headless \
-    --antialiasing \
-    --strategy ${STRATEGY} \
-    --max-cap ${MAX_CAP} \
-    --sh-degree ${SH_DEGREE} \
-    --min-opacity ${MIN_OPACITY} \
-    --init-num-pts ${INIT_POINTS} \
-    --init-extent ${INIT_EXTENT} \
-    > train.log 2>&1 &
+  -d /home/user/data/livingroom \
+  -o output/livingroom_psnr15k \
+  --eval \
+  --save-eval-images \
+  --render-mode RGB \
+  -i 15000 \
+  --gut \
+  --headless \
+  --antialiasing \
+  --strategy mcmc \
+  --max-cap 3000000 \
+  --sh-degree 3 \
+  --min-opacity 0.005 \
+  --init-num-pts 200000 \
+  --init-extent 3.0 \
+  --param-override "init_scaling=0.1,init_opacity=0.1,opacity_reg=0.01,scale_reg=0.01,tv_loss_weight=10,stop_refine=13000,lambda_dssim=0.2" \
+  > train.log 2>&1 &
