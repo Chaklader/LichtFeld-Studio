35a186f4b6ba770bf1468b820edb07e5b23e8466
Chaklader Asfak Arefe <omi.chaklader@gmail.com>
2025-09-21 17:19:18 +0600

    added

--- tools/colmap.sh
+++ tools/colmap.sh
@@ -22,7 +22,7 @@ colmap feature_extractor \
     --ImageReader.single_camera 1 \
     --ImageReader.camera_model PINHOLE \
     --FeatureExtraction.use_gpu 1 \
-    --SiftExtraction.max_num_features 16384 \
+    --SiftExtraction.max_num_features 32768 \
     --SiftExtraction.first_octave -1 \
     --SiftExtraction.num_octaves 4 \
     --SiftExtraction.octave_resolution 3 \
@@ -42,7 +42,7 @@ colmap exhaustive_matcher \
     --SiftMatching.cross_check 1 \
     --SiftMatching.max_ratio 0.75 \
     --SiftMatching.max_distance 0.8 \
-    --FeatureMatching.max_num_matches 16384    
+    --FeatureMatching.max_num_matches 32768    
 
 echo "Waiting for matching to complete..."
 
