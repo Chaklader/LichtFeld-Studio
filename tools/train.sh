ae164024116216e6a7939c890b8428f5720807ef
Chaklader Asfak Arefe <omi.chaklader@gmail.com>
2025-09-21 06:58:48 +0600

    added

--- tools/train.sh
+++ tools/train.sh
@@ -9,7 +9,7 @@ echo "Starting optimized LichtFeld Studio training..."
 # Training parameters - modify these as needed
 # Choose dataset type:
 # COLMAP data (your current):
-DATASET_PATH="/home/user/data/livingroom"
+DATASET_PATH="/home/user/data/living"
 OUTPUT_PATH="output/livingroom"
 
 # Nerfstudio data (for smaller PLY files):
