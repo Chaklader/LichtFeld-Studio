4847daa57705f8282742ce6314a40999fa229314
Chaklader Asfak Arefe <omi.chaklader@gmail.com>
2025-09-25 15:26:27 +0600

    added

--- tools/nerf.sh
+++ tools/nerf.sh
@@ -54,7 +54,8 @@ export QT_QPA_PLATFORM=offscreen
 ns-process-data images \
   --data        "$RAW_DATA_DIR" \
   --output-dir  "$OUTPUT_DIR" \
-  --num-downscales "$NUM_DOWNSCALES" 
+  --num-downscales "$NUM_DOWNSCALES" \
+  --no-gpu
 
 echo "[process_dataset] Conversion finished. Key files:"
 if [[ -f "$OUTPUT_DIR/transforms.json" ]]; then
